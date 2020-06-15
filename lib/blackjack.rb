4cb713af798614019ea9d87af14c64776735e315
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-06-15 17:44:14 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -47,9 +47,6 @@ def invalid_command
  puts "Please enter a valid command"
 end
 
-#####################################################
-# get every test to pass before coding runner below #
-#####################################################
 
 def runner
   welcome 
